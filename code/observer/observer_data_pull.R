db1e6115f88078a5a11f4000c741b79d04bb9ec2
Carly Lovas <clovas@gmri.org>
2026-03-03 10:22:58 -0500

    observer data pull

--- code/observer/observer_data_pull.R
+++ code/observer/observer_data_pull.R
@@ -1,4 +1,12 @@
 # Pull observer data from confidential repository and clean
+library(here)
+library(tidyverse)
+library(gmRi)
+library(readxl)
+library(readr)
+library(matrixStats)
+library(stringi)
+library(data.table)
 
 ## File paths ----
 box_path  <- "/Users/clovas/Library/CloudStorage/Box-Box/"
@@ -28,28 +36,31 @@ catch_data <- tibble(rbindlist(catch[c(8:36)])) |> # this is every year onward
   janitor::clean_names()
 
 
-## Cleaning
+## Cleaning ----
+haul_data |>
+  select(link1, link3, negear, targspec1, targspec2, targspec3, gis_lathbeg, gis_lonhbeg, gis_latsbeg, gis_lonsbeg) |>
+  group_by(link3) |>
+  full_join(catch_data) |>
+  relocate(year, .after = link3) |>
+  relocate(comname, .after = year) |>
+  mutate(kept = ifelse(str_detect(fishdispdesc, "KEPT"), "Kept", "Discarded")) |>
+  select(link3, year, negear, comname, targspec1, targspec2, targspec3, gis_lathbeg, gis_lonhbeg, gis_latsbeg, gis_lonsbeg, hailwt, live_wt, kept) |>
+  filter(!is.na(live_wt)) -> catch_haul
 
-# demo_haul |>
-#   select(link1, link3, negear, targspec1, targspec2, targspec3, gis_lathbeg, gis_lonhbeg, gis_latsbeg, gis_lonsbeg) |>
-#   full_join(demo_catch) |>
-#   relocate(year, .after = link3) |>
-#   relocate(comname, .after = year) |> 
-#   mutate(kept = ifelse(str_detect(fishdispdesc, "KEPT"), "Kept", "Discarded")) |>
-#   select(link3, year, negear, comname, targspec1, targspec2, targspec3, gis_lathbeg, gis_lonhbeg, gis_latsbeg, gis_lonsbeg, hailwt, live_wt, kept) |>
-#   filter(!is.na(live_wt)) -> catch_haul
-# 
-# catch_haul |>
-#   filter(!is.na(gis_latsbeg) & !gis_latsbeg == 1 & (is.na(gis_lathbeg))) |>
-#   select(!c(gis_lathbeg, gis_lonhbeg)) |>
-#   rename(lat = gis_latsbeg,
-#          lon = gis_lonsbeg) -> set_lats # evidently, these are all rod and reel?
-# 
-# catch_haul |>
-#   filter(!link3 %in% set_lats$link3) |>
-#   select(!c(gis_latsbeg, gis_lonsbeg)) |>
-#   rename(lat = gis_lathbeg,
-#          lon = gis_lonhbeg) |>
-#   full_join(set_lats) |>
-#   drop_na() |> 
-#   mutate(decade =  10*year%/%10) -> catch_coords
+catch_haul |>
+  filter(!is.na(gis_latsbeg) & !gis_latsbeg == 1 & (is.na(gis_lathbeg))) |>
+  select(!c(gis_lathbeg, gis_lonhbeg)) |>
+  rename(lat = gis_latsbeg,
+         lon = gis_lonsbeg) -> set_lats # evidently, these are all rod and reel?
+
+catch_haul |>
+  filter(!link3 %in% set_lats$link3) |>
+  select(!c(gis_latsbeg, gis_lonsbeg)) |>
+  rename(lat = gis_lathbeg,
+         lon = gis_lonhbeg) |>
+  full_join(set_lats) |>
+  drop_na() |>
+  mutate(decade =  10*year%/%10) -> catch_coords
+
+## Save out ----
+write_csv(catch_coords, here("data", "processed", "observer_catch_haul_combined.csv"))
